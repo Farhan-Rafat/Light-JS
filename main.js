2296cdc55470c4f33bd0365d76234c21e35229ac
Farhanur Rahman Rafat <farhanrafatdev@gmail.com git config --global user.name farhan-rafat git config --global user.email farhanrafatdev@gmail.com>
2024-01-25 10:42:38 +0600

    my first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function onlight(){
+    document.getElementById('bulb').src='./images/on-light.gif';
+}
+function offlight(){
+    document.getElementById('bulb').src='./images/off-light.gif';
+}
+
